76c4b74a0dae95a5fae90924d3d72f83edc964ef
Enzo-Leal <enzo.leal.09@gmail.com>
2022-09-21 17:08:48 -0300

    falta implementar o buscador de opcodes

--- Compilador.py
+++ Compilador.py
@@ -1,11 +1,9 @@
 # OP CODES
-OP_SOMA = 1000  # Soma 2 numeros e adiciona eles na memoria seguinte
-OP_SUB = 1001  # Subtrai 2 numeros e adiciona eles na memoria seguinte
-OP_FIM = 1999  # Fim do programa
+
 
 # ESPAÇOS DE MEMORIA
-MEM_SOMA = ["1000","0001", "0010", "0011", "0100", "0101", "0110", "0111"]
-MEM_SUB = ["1001","0001", "0010", "0011", "0100", "0101", "0110", "0111"]
+MEM = [0b1000, 0b0001, 0b0010, 0b0011 , 0b0100, 0b0101, 0b0110, 0b0111]
+#MEM_SUB = ["1001", "0001", "0010", "0011", "0100", "0101", "0110", "0111"]
 
 # Contador de programa
 contador_programa = 0
@@ -14,30 +12,37 @@ contador_programa = 0
 def Ciclo_de_instrucoes(ESCOLHA_MEMORIA):
     global contador_programa
 
-    
+    OP_SOMA = 0b1000  # Soma o segundo espaço de memória com o terceiro e armazena o resultado no quarto espaço de memória
+    OP_SUB = 1001  # Subtrai 2 numeros e adiciona eles na memoria seguinte
+    OP_FIM = 1999  # Fim do programa
+
     print("Ciclo de instruções iniciado")
 
-    while contador_programa < len(MEM):
-        print(MEM[contador_programa])
-        if MEM[contador_programa] == int(OP_SOMA):
-            while MEM != OP_FIM:
-                contador_programa += 1
-                MEM[contador_programa +
-                    2] = bin(MEM[contador_programa]) + bin(MEM[contador_programa + 1])
-                contador_programa += 1
-                main()
-
-        elif MEM[contador_programa] == OP_SUB:
-            while MEM != OP_FIM:
-                contador_programa += 1
-                MEM[contador_programa +
-                    2] = bin(MEM[contador_programa]) - bin(MEM[contador_programa + 1])
-                contador_programa += 1
-                main()
+    
+    while True:
+        if ESCOLHA_MEMORIA[contador_programa] == OP_SOMA:
+            print("Soma")
+            MEM[3] = bin(MEM[1] + MEM[2])
+            contador_programa += 1
+            print("MEM_A = ", bin(MEM[0]), "\n MEM_B = ", bin(MEM[1]), "\n MEM_C = ", bin(MEM[2]), "\n MEM_D = ", MEM[3], "\n MEM_E = ", bin(MEM[4]), "\n MEM_F = ", bin(MEM[5]))
+            main()
 
 
-        print("FIM")
+        elif ESCOLHA_MEMORIA[contador_programa] == str(OP_SUB):
+            print("Subtração")
+            MEM[3] = MEM[1] - MEM[2]
+            contador_programa += 1
+            print("MEM_A = ", bin(MEM[0]), "\n MEM_B = ", bin(MEM[1]), "\n MEM_C = ", bin(MEM[2]), "\n MEM_D = ", MEM[3], "\n MEM_E = ", bin(MEM[4]), "\n MEM_F = ", bin(MEM[5]))
 
+        elif ESCOLHA_MEMORIA[contador_programa] == str(OP_FIM):
+            print("Fim")
+            break
+
+        else:
+            print("Instrução inválida")
+            break
+
+   
 
 def main():
     print("Compilador iniciado\n Digite a instrução desejada\n\n\n 1 - Soma\n 2 - Subtração\n 3 - DUMP de memoria 4 - FIM")
@@ -47,7 +52,7 @@ def main():
 
         match OPCAO_MENU:
             case "1":
-                Ciclo_de_instrucoes(MEM_SOMA)
+                Ciclo_de_instrucoes(MEM)
                 main()
             case "2":
                 Ciclo_de_instrucoes(MEM_SUB)
@@ -55,11 +60,11 @@ def main():
             case "3":
                 print("DUMP de memoria\n MEM_A = ", MEM[0], "\n MEM_B = ", MEM[1], "\n MEM_C = ", MEM[2], "\n MEM_D = ", MEM[3], "\n MEM_E = ",
                       MEM[4], "\n MEM_F = ", MEM[5], "\n MEM_G = ", MEM[6], "\n MEM_H = ", MEM[7], "\n MEM_I = ", MEM[8], "\n MEM_J = ")
-            
+
             case "4":
                 print("FIM")
                 break
-            
+
             case _:
                 print("Opção inválida")
 

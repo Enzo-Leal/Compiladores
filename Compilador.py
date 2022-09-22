3a752b686c778b9163b2b7c9aa8b861c46a8ffc3
Enzo Leal <enzo.leal.09@gmail.com>
2022-09-22 00:17:10 -0300

    .

--- Compilador.py
+++ Compilador.py
@@ -7,7 +7,7 @@
 #opcao fazer um programa só, fazendo um ponto no meio pra exibir i resultado
 
 #ideia de OPCODE, zerar um espaço de memoria, lipar as memorias
-
+#gerar um erro e gravar em um arquivo de log em um espaço de memoria
 
 
 # OP CODES
@@ -16,6 +16,10 @@
 # ESPAÇOS DE MEMORIA
 MEM = [0b1000, 0b0001, 0b0010, 0b0000 , 0b0100, 0b0101, 0b0110, 0b0111]
 #MEM_SUB = ["1001", "0001", "0010", "0011", "0100", "0101", "0110", "0111"]
+
+MEM_TXT = []
+
+MEM_CONVERSAO  = open("instrucoes.txt", "r")
 
 # Contador de programa
 contador_programa = 0
@@ -31,13 +35,13 @@ def Ciclo_de_instrucoes(ESCOLHA_MEMORIA):
     print("Ciclo de instruções iniciado")
 
     
-    while True:
+    while ESCOLHA_MEMORIA[contador_programa] != OP_FIM:
         
         if ESCOLHA_MEMORIA[contador_programa] == OP_SOMA:
             print("Soma")
             MEM[3] = bin(MEM[1] + MEM[2])
             contador_programa += 1
-            print(" MEM_A = ", bin(MEM[0]), "\n MEM_B = ", bin(MEM[1]), "\n MEM_C = ", bin(MEM[2]), "\n MEM_D = ", MEM[3], "\n MEM_E = ", bin(MEM[4]), "\n MEM_F = ", bin(MEM[5]))
+            print(" MEM_A = ", bin(MEM[0]), "\n MEM_B = ", bin(MEM[1]), "\n MEM_C = ", bin(MEM[2]), "\n MEM_D = ", MEM[3])
             main()
 
 
@@ -57,7 +61,20 @@ def Ciclo_de_instrucoes(ESCOLHA_MEMORIA):
 
    
 
+#funcao para ler o arquivo e colocar em um array de "memoria"
+
+def TXT_PARA_MEM():
+    for linha in MEM_CONVERSAO:
+        MEM_TXT.append(linha)
+
+
+
+
+
 def main():
+    
+    TXT_PARA_MEM()
+
     print("Compilador iniciado\n Digite a instrução desejada\n\n\n 1 - Soma\n 2 - Subtração\n 3 - DUMP de memoria \n 4 - FIM")
 
     while True:
@@ -65,7 +82,7 @@ def main():
 
         match OPCAO_MENU:
             case "1":
-                Ciclo_de_instrucoes(MEM)
+                Ciclo_de_instrucoes(MEM_TXT)
                 main()
             case "2":
                 Ciclo_de_instrucoes(MEM_SUB)
@@ -77,6 +94,10 @@ def main():
             case "4":
                 print("FIM")
                 break
+
+            case "5":
+                print(MEM_TXT)
+                print(type(MEM_TXT[0]))
 
             case _:
                 print("Opção inválida")

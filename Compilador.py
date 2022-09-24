eee1db3f193489a9a6c566a7185fb19f680555a0
Enzo-Leal <enzo.leal.09@gmail.com>
2022-09-24 15:31:30 -0300

    .

--- Compilador.py
+++ Compilador.py
@@ -1,106 +1,100 @@
-#nao pode pedir imput, tem que roodar direto
-#ler de um arquivo 
+# nao pode pedir imput, tem que roodar direto
+# ler de um arquivo
 
 
-#na atividade 2 traduzir um arquivo para o trabalho 1
+# na atividade 2 traduzir um arquivo para o trabalho 1
 
-#opcao fazer um programa só, fazendo um ponto no meio pra exibir i resultado
+# opcao fazer um programa só, fazendo um ponto no meio pra exibir i resultado
 
-#ideia de OPCODE, zerar um espaço de memoria, lipar as memorias
-#gerar um erro e gravar em um arquivo de log em um espaço de memoria
+# ideia de OPCODE, zerar um espaço de memoria, lipar as memorias
+# gerar um erro e gravar em um arquivo de log em um espaço de memoria
+
+from winreg import REG_BINARY
 
 
-# OP CODES
-
-
-# ESPAÇOS DE MEMORIA
-MEM = [0b1000, 0b0001, 0b0010, 0b0000 , 0b0100, 0b0101, 0b0110, 0b0111]
-#MEM_SUB = ["1001", "0001", "0010", "0011", "0100", "0101", "0110", "0111"]
-
-MEM_TXT = []
-
-MEM_CONVERSAO  = open("instrucoes.txt", "r")
-
+MEM_INTRUCOES = []
 # Contador de programa
 contador_programa = 0
 
+# funcao para ler o arquivo e colocar em um array de "memoria"
 
-def Ciclo_de_instrucoes(ESCOLHA_MEMORIA):
+
+def MEM_BIN_PARA_MEM_INSTRUCOES():
+
+    with open("instrucoes.bin", "r") as instrucoes:
+        for linha in instrucoes:
+            MEM_INTRUCOES.append(linha)
+
+
+# while MEM_INTRUCOES:
+    #    print(MEM_INTRUCOES)
+    #    MEM_INTRUCOES = file.read(8)
+
+
+    #with open("instrucoes.bin", "rb") as instrucoes:
+    #    for linha in instrucoes:
+    #        MEM_INTRUCOES.append(linha.rstrip())
+
+    print(MEM_INTRUCOES)
+    print(type(MEM_INTRUCOES[0]))
+    
+
+
+def LER_INSTRUCAO():
     global contador_programa
 
-    OP_SOMA = 0b1000  # Soma o segundo espaço de memória com o terceiro e armazena o resultado no quarto espaço de memória
-    OP_SUB = 1001  # Subtrai 2 numeros e adiciona eles na memoria seguinte
-    OP_FIM = 1999  # Fim do programa
+    TAMANHO_MEM_INTRUCOES = len(MEM_INTRUCOES)
 
-    print("Ciclo de instruções iniciado")
+    OP_SOMA = b'10000001'
+    OP_SUB = 9998
+    OP_MULT = 9997
+    OP_DIV = 9996
 
-    
-    while ESCOLHA_MEMORIA[contador_programa] != OP_FIM:
-        
-        if ESCOLHA_MEMORIA[contador_programa] == OP_SOMA:
-            print("Soma")
-            MEM[3] = bin(MEM[1] + MEM[2])
-            contador_programa += 1
-            print(" MEM_A = ", bin(MEM[0]), "\n MEM_B = ", bin(MEM[1]), "\n MEM_C = ", bin(MEM[2]), "\n MEM_D = ", MEM[3])
-            main()
-
-
-        elif ESCOLHA_MEMORIA[contador_programa] == str(OP_SUB):
-            print("Subtração")
-            MEM[3] = MEM[1] - MEM[2]
-            contador_programa += 1
-            print("MEM_A = ", bin(MEM[0]), "\n MEM_B = ", bin(MEM[1]), "\n MEM_C = ", bin(MEM[2]), "\n MEM_D = ", MEM[3], "\n MEM_E = ", bin(MEM[4]), "\n MEM_F = ", bin(MEM[5]))
-
-        elif ESCOLHA_MEMORIA[contador_programa] == str(OP_FIM):
-            print("Fim")
+    while contador_programa < TAMANHO_MEM_INTRUCOES:
+        if MEM_INTRUCOES[contador_programa] == OP_SOMA:
+            SOMA()
             break
-
+        elif MEM_INTRUCOES[contador_programa] == OP_SUB:
+            SUB()
+            break
+        elif MEM_INTRUCOES[contador_programa] == OP_MULT:
+            MULT()
+            break
+        elif MEM_INTRUCOES[contador_programa] == OP_DIV:
+            DIV()
+            break
         else:
-            print("Instrução inválida")
+            contador_programa += 1
+            LER_INSTRUCAO()
             break
 
-   
 
-#funcao para ler o arquivo e colocar em um array de "memoria"
+def SOMA():
+    global contador_programa
+    contador_programa += 1
+    REG_A = bin(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    REG_B = bin(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    MEM_INTRUCOES[contador_programa] = bytes(REG_A) + bytes(REG_B)
+    contador_programa += 1
 
-def TXT_PARA_MEM():
-    for linha in MEM_CONVERSAO:
-        MEM_TXT.append(linha)
+    print("DUMP da memoria após soma\n")
+    print(MEM_INTRUCOES)
 
-
-
+    LER_INSTRUCAO()
 
 
 def main():
-    
-    TXT_PARA_MEM()
+    global contador_programa
 
-    print("Compilador iniciado\n Digite a instrução desejada\n\n\n 1 - Soma\n 2 - Subtração\n 3 - DUMP de memoria \n 4 - FIM")
+    MEM_BIN_PARA_MEM_INSTRUCOES()
 
-    while True:
-        OPCAO_MENU = input("Digite a opção desejada: ")
+    print("DUMP da memoria inicial\n")
+    print(MEM_INTRUCOES)
 
-        match OPCAO_MENU:
-            case "1":
-                Ciclo_de_instrucoes(MEM_TXT)
-                main()
-            case "2":
-                Ciclo_de_instrucoes(MEM_SUB)
-
-            case "3":
-                print("DUMP de memoria\n MEM_A = ", MEM[0], "\n MEM_B = ", MEM[1], "\n MEM_C = ", MEM[2], "\n MEM_D = ", MEM[3], "\n MEM_E = ",
-                      MEM[4], "\n MEM_F = ", MEM[5], "\n MEM_G = ", MEM[6], "\n MEM_H = ", MEM[7], "\n MEM_I = ", MEM[8], "\n MEM_J = ")
-
-            case "4":
-                print("FIM")
-                break
-
-            case "5":
-                print(MEM_TXT)
-                print(type(MEM_TXT[0]))
-
-            case _:
-                print("Opção inválida")
+    print("Compilador iniciado")
+    LER_INSTRUCAO()
 
 
 main()

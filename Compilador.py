c93a48387e773ff837259b06fdc2b42879caee78
Enzo-Leal <enzo.leal.09@gmail.com>
2022-09-25 16:07:35 -0300

    funcionando corretamente, falta ainda editar as funcionalidades e tirar os prints

--- Compilador.py
+++ Compilador.py
@@ -9,14 +9,140 @@
 # ideia de OPCODE, zerar um espaço de memoria, lipar as memorias
 # gerar um erro e gravar em um arquivo de log em um espaço de memoria
 
-#from winreg import REG_BINARY
+
+
 
 
 MEM_INTRUCOES = []
 # Contador de programa
 contador_programa = 0
 
-# funcao para ler o arquivo e colocar em um array de "memoria"
+def CONVERTER_RESULTADO(RESULTADO):
+    match RESULTADO:
+        case 0:
+            return "00000000"
+        case 1:
+            return "00000001"
+        case 2:
+            return "00000010"
+        case 3:
+            return "00000011"
+        case 4:
+            return "00000100"
+        case 5:
+            return "00000101"
+        case 6:
+            return "00000110"
+        case 7:
+            return "00000111"
+        case 8:
+            return "00001000"
+        case 9:
+            return "00001001"
+        case 10:
+            return "00001010"
+        case 11:
+            return "00001011"
+        case 12:
+            return "00001100"
+        case 13:
+            return "00001101"
+        case 14:
+            return "00001110"
+        case 15:
+            return "00001111"
+        case 16:
+            return "00010000"
+        case 17:
+            return "00010001"
+        case 18:
+            return "00010010"
+        case 19:
+            return "00010011"
+        case 20:
+            return "00010100"
+        case 21:
+            return "00010101"
+        case 22:
+            return "00010110"
+        case 23:
+            return "00010111"
+        case 24:
+            return "00011000"
+        case 25:
+            return "00011001"
+        case 26:
+            return "00011010"
+        case 27:
+            return "00011011"
+        case 28:
+            return "00011100"
+        case 29:
+            return "00011101"
+        case 30:
+            return "00011110"
+        case 31:
+            return "00011111"
+        case 32:
+            return "00100000"
+        case 33:
+            return "00100001"
+        case 34:
+            return "00100010"
+        case 35:
+            return "00100011"
+        case 36:
+            return "00100100"
+        case 37:
+            return "00100101"
+        case 38:
+            return "00100110"
+        case 39:
+            return "00100111"
+        case 40:
+            return "00101000"
+        case 41:
+            return "00101001"
+        case 42:
+            return "00101010"
+        case 43:
+            return "00101011"
+        case 44:
+            return "00101100"
+        case 45:
+            return "00101101"
+        case 46:
+            return "00101110"
+        case 47:
+            return "00101111"
+        case 48:
+            return "00110000"
+        case 49:
+            return "00110001"
+        case 50:
+            return "00110010"
+        case 51:
+            return "00110011"
+        case 52:
+            return "00110100"
+        case 53:
+            return "00110101"
+        case 54:
+            return "00110110"
+        case 55:
+            return "00110111"
+        case 56:
+            return "00111000"
+        case 57:
+            return "00111001"
+        case 58:
+            return "00111010"
+        case 59:
+            return "00111011"
+        case 60:
+            return "00111100"
+        
+
 
 
 def MEM_BIN_PARA_MEM_INSTRUCOES():
@@ -24,22 +150,7 @@ def MEM_BIN_PARA_MEM_INSTRUCOES():
     with open("instrucoes_txt.txt", "r") as instrucoes:
         
         for linha in instrucoes:
-            MEM_INTRUCOES.append(linha)
-            for i in range(len(MEM_INTRUCOES)):
-                MEM_INTRUCOES[i] 
-
-# while MEM_INTRUCOES:
-    #    print(MEM_INTRUCOES)
-    #    MEM_INTRUCOES = file.read(8)
-
-
-    #with open("instrucoes.bin", "rb") as instrucoes:
-    #    for linha in instrucoes:
-    #        MEM_INTRUCOES.append(linha.rstrip())
-
-    print(MEM_INTRUCOES)
-    print(type(MEM_INTRUCOES[0]))
-    
+            MEM_INTRUCOES.append(linha.strip())    
 
 
 def LER_INSTRUCAO():
@@ -47,24 +158,42 @@ def LER_INSTRUCAO():
 
     TAMANHO_MEM_INTRUCOES = len(MEM_INTRUCOES)
 
-    OP_SOMA = b'10000001'
-    OP_SUB = 9998
-    OP_MULT = 9997
-    OP_DIV = 9996
+    OP_SOMA = 10000001
+    OP_SUB = 10000010
+    OP_MULT = 10000011
+    OP_DIV = 10000100
+    OP_JMP = 10000101
+    OP_CLEAR_MEM = 10000110
+    OP_EXIT = 11111111
 
-    while contador_programa < TAMANHO_MEM_INTRUCOES:
-        if MEM_INTRUCOES[contador_programa] == OP_SOMA:
+    while contador_programa <= TAMANHO_MEM_INTRUCOES:
+        print("contador de programa: ", contador_programa)
+
+        if MEM_INTRUCOES[contador_programa] == str(OP_SOMA):
             SOMA()
             break
-        elif MEM_INTRUCOES[contador_programa] == OP_SUB:
+        elif MEM_INTRUCOES[contador_programa] == str(OP_SUB):
             SUB()
             break
-        elif MEM_INTRUCOES[contador_programa] == OP_MULT:
+        elif MEM_INTRUCOES[contador_programa] == str(OP_MULT):
             MULT()
             break
-        elif MEM_INTRUCOES[contador_programa] == OP_DIV:
+        elif MEM_INTRUCOES[contador_programa] == str(OP_DIV):
             DIV()
             break
+        elif MEM_INTRUCOES[contador_programa] == str(OP_JMP):
+            JMP()
+            break
+        elif MEM_INTRUCOES[contador_programa] == str(OP_CLEAR_MEM):
+            CLEAR_MEM()
+            break
+        elif MEM_INTRUCOES[contador_programa] == str(OP_EXIT):
+            #colocar aqui a funcao escrever no arquivo de saida
+            with open("output.txt", "w") as txt_file:
+                for line in MEM_INTRUCOES:
+                    txt_file.write("".join(line) + "\n")
+            break
+
         else:
             contador_programa += 1
             LER_INSTRUCAO()
@@ -74,18 +203,75 @@ def LER_INSTRUCAO():
 def SOMA():
     global contador_programa
     contador_programa += 1
-    REG_A = bin(MEM_INTRUCOES[contador_programa])
+    REG_A = int(MEM_INTRUCOES[contador_programa])
     contador_programa += 1
-    REG_B = bin(MEM_INTRUCOES[contador_programa])
+    REG_B = int(MEM_INTRUCOES[contador_programa])
     contador_programa += 1
-    MEM_INTRUCOES[contador_programa] = bytes(REG_A) + bytes(REG_B)
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A + REG_B) 
     contador_programa += 1
 
     print("DUMP da memoria após soma\n")
     print(MEM_INTRUCOES)
+    print(contador_programa)
+    LER_INSTRUCAO()
+
+def SUB():
+    global contador_programa
+    contador_programa += 1
+    REG_A = int(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    REG_B = int(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A - REG_B) 
+    contador_programa += 1
+
+    print("DUMP da memoria após sub\n")
+    print(MEM_INTRUCOES)
 
     LER_INSTRUCAO()
 
+def MULT():
+    global contador_programa
+    contador_programa += 1
+    REG_A = int(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    REG_B = int(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A * REG_B) 
+    contador_programa += 1
+
+    print("DUMP da memoria após mult\n")
+    print(MEM_INTRUCOES)
+
+    LER_INSTRUCAO()
+
+def DIV():
+    global contador_programa
+    contador_programa += 1
+    REG_A = int(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    REG_B = int(MEM_INTRUCOES[contador_programa])
+    contador_programa += 1
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A / REG_B) 
+    contador_programa += 1
+
+    print("DUMP da memoria após div\n")
+    print(MEM_INTRUCOES)
+
+    LER_INSTRUCAO()
+
+def JMP():
+    global contador_programa
+    contador_programa += 1
+    contador_programa = int(MEM_INTRUCOES[contador_programa])
+    LER_INSTRUCAO()
+
+def CLEAR_MEM():
+    global contador_programa
+    contador_programa += 1
+    MEM_INTRUCOES[contador_programa] = "00000000"
+    contador_programa += 1
+    LER_INSTRUCAO()
 
 def main():
     global contador_programa

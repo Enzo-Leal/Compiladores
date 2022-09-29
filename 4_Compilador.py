4189569ad1995307df344987be50e6d8d74ff8c7
Enzo Leal <enzo.leal.09@gmail.com>
2022-09-28 21:46:41 -0300

    bugfix²

--- 4_Compilador.py
+++ 4_Compilador.py
@@ -263,6 +263,266 @@ def CONVERTER_RESULTADO(RESULTADO):
         case 127:
             return "01111111"
 
+def CONVERTER_BINARIO_DECIMAL(binario):
+    match binario:
+        case "00000000":
+            return 0
+        case "00000001":
+            return 1
+        case "00000010":
+            return 2
+        case "00000011":
+            return 3
+        case "00000100":
+            return 4
+        case "00000101":
+            return 5
+        case "00000110":
+            return 6
+        case "00000111":
+            return 7
+        case "00001000":
+            return 8
+        case "00001001":
+            return 9
+        case "00001010":
+            return 10
+        case "00001011":
+            return 11
+        case "00001100":
+            return 12
+        case "00001101":
+            return 13
+        case "00001110":
+            return 14
+        case "00001111":
+            return 15
+        case "00010000":
+            return 16
+        case "00010001":
+            return 17
+        case "00010010":
+            return 18
+        case "00010011":
+            return 19
+        case "00010100":
+            return 20
+        case "00010101":
+            return 21
+        case "00010110":
+            return 22
+        case "00010111":
+            return 23
+        case "00011000":
+            return 24
+        case "00011001":
+            return 25
+        case "00011010":
+            return 26
+        case "00011011":
+            return 27
+        case "00011100":
+            return 28
+        case "00011101":
+            return 29
+        case "00011110":
+            return 30
+        case "00011111":
+            return 31
+        case "00100000":
+            return 32
+        case "00100001":
+            return 33
+        case "00100010":
+            return 34
+        case "00100011":
+            return 35
+        case "00100100":
+            return 36
+        case "00100101":
+            return 37
+        case "00100110":
+            return 38
+        case "00100111":
+            return 39
+        case "00101000":
+            return 40
+        case "00101001":
+            return 41
+        case "00101010":
+            return 42
+        case "00101011":
+            return 43
+        case "00101100":
+            return 44
+        case "00101101":
+            return 45
+        case "00101110":
+            return 46
+        case "00101111":
+            return 47
+        case "00110000":
+            return 48
+        case "00110001":
+            return 49
+        case "00110010":
+            return 50
+        case "00110011":
+            return 51
+        case "00110100":
+            return 52
+        case "00110101":
+            return 53
+        case "00110110":
+            return 54
+        case "00110111":
+            return 55
+        case "00111000":
+            return 56
+        case "00111001":
+            return 57
+        case "00111010":
+            return 58
+        case "00111011":
+            return 59
+        case "00111100":
+            return 60
+        case "00111101":
+            return 61
+        case "00111110":
+            return 62
+        case "00111111":
+            return 63
+        case "01000000":
+            return 64
+        case "01000001":
+            return 65
+        case "01000010":
+            return 66
+        case "01000011":
+            return 67
+        case "01000100":
+            return 68
+        case "01000101":
+            return 69
+        case "01000110":
+            return 70
+        case "01000111":
+            return 71
+        case "01001000":
+            return 72
+        case "01001001":
+            return 73
+        case "01001010":
+            return 74
+        case "01001011":
+            return 75
+        case "01001100":
+            return 76
+        case "01001101":
+            return 77
+        case "01001110":
+            return 78
+        case "01001111":
+            return 79
+        case "01010000":
+            return 80
+        case "01010001":
+            return 81
+        case "01010010":
+            return 82
+        case "01010011":
+            return 83
+        case "01010100":
+            return 84
+        case "01010101":
+            return 85
+        case "01010110":
+            return 86
+        case "01010111":
+            return 87
+        case "01011000":
+            return 88
+        case "01011001":
+            return 89
+        case "01011010":
+            return 90
+        case "01011011":
+            return 91
+        case "01011100":
+            return 92
+        case "01011101":
+            return 93
+        case "01011110":
+            return 94
+        case "01011111":
+            return 95
+        case "01100000":
+            return 96
+        case "01100001":
+            return 97
+        case "01100010":
+            return 98
+        case "01100011":
+            return 99
+        case "01100100":
+            return 100
+        case "01100101":
+            return 101
+        case "01100110":
+            return 102
+        case "01100111":
+            return 103
+        case "01101000":
+            return 104
+        case "01101001":
+            return 105
+        case "01101010":
+            return 106
+        case "01101011":
+            return 107
+        case "01101100":
+            return 108
+        case "01101101":
+            return 109
+        case "01101110":
+            return 110
+        case "01101111":
+            return 111
+        case "01110000":
+            return 112
+        case "01110001":
+            return 113
+        case "01110010":
+            return 114
+        case "01110011":
+            return 115
+        case "01110100":
+            return 116
+        case "01110101":
+            return 117
+        case "01110110":
+            return 118
+        case "01110111":
+            return 119
+        case "01111000":
+            return 120
+        case "01111001":
+            return 121
+        case "01111010":
+            return 122
+        case "01111011":
+            return 123
+        case "01111100":
+            return 124
+        case "01111101":
+            return 125
+        case "01111110":
+            return 126
+        case "01111111":
+            return 127
+
+
 #Função que pegar as instrucoes do arquivo "3_saida_montador.txt" e adiciona ao array MEM_INSTRUCOES
 def MEM_BIN_PARA_MEM_INSTRUCOES():
 
@@ -275,11 +535,11 @@ def MEM_BIN_PARA_MEM_INSTRUCOES():
 def SOMA():
     global contador_programa
     contador_programa += 1
-    REG_A = int(MEM_INTRUCOES[contador_programa])
+    REG_A = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    REG_B = int(MEM_INTRUCOES[contador_programa])
+    REG_B = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A + REG_B)
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(int(CONVERTER_BINARIO_DECIMAL(REG_A)) + int(CONVERTER_BINARIO_DECIMAL(REG_B)))
     contador_programa += 1
     LER_INSTRUCAO()
 
@@ -287,11 +547,11 @@ def SOMA():
 def SUB():
     global contador_programa
     contador_programa += 1
-    REG_A = int(MEM_INTRUCOES[contador_programa])
+    REG_A = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    REG_B = int(MEM_INTRUCOES[contador_programa])
+    REG_B = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A - REG_B)
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(int(CONVERTER_BINARIO_DECIMAL(REG_A)) + int(CONVERTER_BINARIO_DECIMAL(REG_B)))
     contador_programa += 1
     LER_INSTRUCAO()
 
@@ -299,11 +559,11 @@ def SUB():
 def MULT():
     global contador_programa
     contador_programa += 1
-    REG_A = int(MEM_INTRUCOES[contador_programa])
+    REG_A = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    REG_B = int(MEM_INTRUCOES[contador_programa])
+    REG_B = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A * REG_B)
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(int(CONVERTER_BINARIO_DECIMAL(REG_A)) + int(CONVERTER_BINARIO_DECIMAL(REG_B)))
     contador_programa += 1
     LER_INSTRUCAO()
 
@@ -311,11 +571,11 @@ def MULT():
 def DIV():
     global contador_programa
     contador_programa += 1
-    REG_A = int(MEM_INTRUCOES[contador_programa])
+    REG_A = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    REG_B = int(MEM_INTRUCOES[contador_programa])
+    REG_B = MEM_INTRUCOES[contador_programa]
     contador_programa += 1
-    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(REG_A / REG_B)
+    MEM_INTRUCOES[contador_programa] = CONVERTER_RESULTADO(int(CONVERTER_BINARIO_DECIMAL(REG_A)) + int(CONVERTER_BINARIO_DECIMAL(REG_B)))
     contador_programa += 1
     LER_INSTRUCAO()
 
@@ -381,7 +641,7 @@ def LER_INSTRUCAO():
             LER_INSTRUCAO()
             break
 
-        
+
 def main():
     print("Programa iniciado")
     MEM_BIN_PARA_MEM_INSTRUCOES()
